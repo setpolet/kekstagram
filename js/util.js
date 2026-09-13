0c967721fa0b09f5a915e872a937f56841b0d5f0
setpolet <setpolet@gmail.com>
2026-09-13 13:32:30 +0300

    Remove unused utility functions

--- js/util.js
+++ js/util.js
@@ -1,9 +1,3 @@
-export const getRandomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
-
-export const getRandomArrayElement = (elements) =>
-  elements[getRandomInt(0, elements.length - 1)];
-
 export const debounce = (callback, timeoutDelay = 500) => {
   let timeoutId;
 
